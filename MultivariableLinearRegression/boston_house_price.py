36779a2c4b1667c0aee05a35b3993d372cd957de
shyam <shyamjithsmaniyoor@gmail.com>
2019-12-29 21:36:28 +0530

    moving to multivariable regression

--- MultivariableLinearRegression/boston_house_price.py
+++ MultivariableLinearRegression/boston_house_price.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_boston
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 boston_dataset = load_boston()
 pd.set_option('display.width', 320)
@@ -16,7 +17,7 @@ class BostonHouse:
         dataset_return = dir(self.boston_dataset)
         print(dataset_return)
         # getting description
-        # print(boston_dataset.DESCR)
+        print(boston_dataset.DESCR)
         # accessing data
         print(boston_dataset.data.shape)
         print(type(boston_dataset.data))
@@ -31,40 +32,74 @@ class BostonHouse:
         print(pd.isnull(data).any())
         self.boston_dataset = data
 
-    def visualization(self):
+    def visualization(self, mask):
         """visualizing data Histogram, Distribution abd Bar Chart"""
         plt.figure(figsize=[10, 6])
         plt.ylabel('No. of Houses')
         plt.xlabel('Price in Thousands')
         plt.hist(self.boston_dataset['PRICE'], bins=50, ec='black', color='#46FFD1')
-        plt.show()
+        # plt.show()
         plt.figure(figsize=[10, 6])
         sns.distplot(self.boston_dataset['PRICE'], bins=50, color='#fbc02d')
-        plt.show()
+        # plt.show()
         # number of rooms visualization
         plt.figure(figsize=[10, 6])
         plt.ylabel('No. of Houses')
         plt.xlabel('Average Rooms House')
         plt.hist(self.boston_dataset['RM'], ec='black', color='#BAFF3E')
-        plt.show()
+        # plt.show()
         plt.figure(figsize=[10, 6])
         sns.distplot(self.boston_dataset['RM'], color='#fbc02d')
-        plt.show()
+        # plt.show()
         # number of rooms visualization
         plt.figure(figsize=[10, 6])
         plt.ylabel('No. of Houses')
         plt.xlabel('accessibility to highways')
         plt.hist(self.boston_dataset['RAD'], bins=24, ec='black', color='#3BC3FF')
-        plt.show()
+        # plt.show()
         plt.figure(figsize=[10, 6])
         sns.distplot(self.boston_dataset['RM'], color='#3BC3FF')
-        plt.show()
+        # plt.show()
         # hard coding bar diagram
         frequency = self.boston_dataset['RAD'].value_counts()
         plt.figure(figsize=[10, 6])
         plt.ylabel('No. of Houses')
         plt.xlabel('accessibility to highways')
         plt.bar(frequency.index, height=frequency)
+        # plt.show()
+        # heat ma on correlation using seaborn
+        plt.figure(figsize=[16, 10])
+        sns.heatmap(self.boston_dataset.corr(), mask=mask, annot=True)
+        plt.show()
+        # scatter plot
+        plt.figure(figsize=[10, 6])
+        nox_dis_correlation = self.boston_dataset['NOX'].corr(self.boston_dataset['DIS'])
+        plt.title(f'NOX, DIS With (Correlation {nox_dis_correlation})')
+        plt.xlabel('DIS-distance from employment')
+        plt.ylabel('NOX-nitric oxide pollution')
+        plt.scatter(x=self.boston_dataset['DIS'], y=self.boston_dataset['NOX'], alpha=0.5, s=50, color='indigo')
+        plt.show()
+        # scatter plot using seaborn
+        sns.set()  # set default styling
+        sns.set_style('whitegrid')
+        sns.set_context('talk')
+        sns.jointplot(x=self.boston_dataset['DIS'], y=self.boston_dataset['NOX'], kind='hex', color='indigo',
+                      )
+        sns.jointplot(x=self.boston_dataset['TAX'], y=self.boston_dataset['RAD'], color='darkred', joint_kws={
+            'alpha': 0.5})
+        # lmplot will auto plot linear line
+        sns.lmplot(x='TAX', y='RAD', data=self.boston_dataset)
+        # plot the RM vs PRICE correlation
+        rm_price_correlation = self.boston_dataset['RM'].corr(self.boston_dataset['PRICE'])
+        plt.title(f'RM, PRICE With (Correlation {rm_price_correlation})')
+        plt.xlabel('RM-median number of rooms')
+        plt.ylabel('PRICE-property Price in thousands')
+        plt.scatter(x=self.boston_dataset['RM'], y=self.boston_dataset['PRICE'], alpha=0.5, s=50, color='indigo')
+        # lmplot will auto plot linear line
+        sns.lmplot(x='RM', y='PRICE', data=self.boston_dataset)
+        plt.show()
+        # ploting enter data frame using seaborn pairplot
+        sns.pairplot(self.boston_dataset, kind='reg', plot_kws={'line_kws': {'color': 'cyan'}})
         plt.show()
 
     def static_data(self):
@@ -79,10 +114,25 @@ class BostonHouse:
         print('larges house price')
         print(self.boston_dataset['PRICE'].max())
         print(self.boston_dataset.describe())
+        # correlation 1.0 or -1.0 perfect correlation
+        # correlation between room and house price
+        print('correlation between room and house price')
+        print(self.boston_dataset['PRICE'].corr(self.boston_dataset['RM']))
+        print('correlation between house price and teaching ratio')
+        print(self.boston_dataset['PRICE'].corr(self.boston_dataset['PTRATIO']))
+        print('correlation od data`s')
+        print(self.boston_dataset.corr(method='pearson'))
+        # creating dumpy array like above to feed data
+        mask = np.zeros_like(self.boston_dataset.corr())
+        # creating triangle model
+        triangle_indices = np.triu_indices_from(mask)
+        mask[triangle_indices] = True
+        boston.visualization(mask)
+        print(mask)
 
 
 boston = BostonHouse(boston_dataset)
 boston.info()
 boston.cleaning_data()
-# boston.visualization()
+
 boston.static_data()
